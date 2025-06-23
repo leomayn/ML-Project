04deaf13a0de0ac61132e783df5cba2d52e97a78
Leo Maynard-Zhang <leomaynard-zhang@ucsb.edu>
2025-06-22 22:20:12 -0700

    add game folder and agent folder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ML-Project
+
+This project is split into two parts: Game and agent who plays the game
---
+++ game/game1/game1main.py
@@ -0,0 +1,7 @@
+starting_money = 10
+redraw_cost = 2
+interest_factor = 2
+low_value = 1
+medium_value = 2
+high_value = 3
+number_of_rounds = 3
